5d76290bcc71a9548351e366ca18c723581813c6
Viktoria <avramovaviki017@gmail.com>
2025-10-14 20:05:13 +0300

    Created a class and stared with the welcome message

---
+++ src/GuessTheNumber.java
@@ -0,0 +1,6 @@
+public class GuessTheNumber {
+    public static void main(String[] args){
+        System.out.println("Welcome to Guess the Number!");
+        System.out.println("Let`s start!");
+    }
+}
