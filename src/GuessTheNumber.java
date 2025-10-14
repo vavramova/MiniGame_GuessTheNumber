1eeb3dad4712dd9f79cbf246ebabcf1d41946f9b
Viktoria <avramovaviki017@gmail.com>
2025-10-14 20:09:46 +0300

    Generated a number for the Computer to remember using Math.random()

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,6 +1,12 @@
+/**
+ *
+ */
 public class GuessTheNumber {
     public static void main(String[] args){
         System.out.println("Welcome to Guess the Number!");
         System.out.println("Let`s start!");
+
+        int secretNum = (int)(Math.random() * 100) + 1;
+        System.out.println("I generated a number!");
     }
 }
