0ff8b79259ced0e609dfefb1277322962299a263
Viktoria <avramovaviki017@gmail.com>
2025-10-14 20:15:32 +0300

    reading the users guess

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 /**
  *
  */
 public class GuessTheNumber {
     public static void main(String[] args){
+        //Welcome message
         System.out.println("Welcome to Guess the Number!");
         System.out.println("Let`s start!");
 
+        //Generating a random number
         int secretNum = (int)(Math.random() * 100) + 1;
         System.out.println("I generated a number!");
+
+        //Reading user`s Number
+        System.out.println("Now it`s your turn to enter a number");
+        Scanner userInput = new Scanner(System.in);
+        int userGuess = userInput.nextInt();
+
+        System.out.println("Your guess: " + userGuess);
+
+
+
+
     }
 }
