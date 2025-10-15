f48a2ecbe76bba378c7c4972537e2be5a8480dc4
Viktoria <avramovaviki017@gmail.com>
2025-10-15 09:42:27 +0300

    started JFrame

---
+++ src/MyFrameGSSN.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MyFrameGSSN extends JFrame {
+    private JTextField inputField;
+    private JLabel msgLabel;
+    private JButton guessButton;
+
+    public MyFrameGSSN(){
+        setTitle("Guess the Number");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        setSize(420,420);
+        setVisible(true);
+
+
+    }
+}
